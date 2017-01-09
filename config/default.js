3b18cb2e5db649b90a676a1a01ae33453e585596
Fengming Jiang <fenivana@noindoin.com>
2017-01-09 16:23:58 +0800

    forgot to delete old codes in config/default.js - -!

--- config/default.js
+++ config/default.js
@@ -8,17 +8,6 @@ module.exports = {
 
   publicPath: '/assets/',
   serveStaticMountPath: '/assets'
-}
-module.exports = {
-  runtimeConfig: {
-    settingFoo: true,
-    settingBar: false
-  },
-
-  ssrPort: 8200,
-
-  publicPath: '/assets/',
-  serveStaticMountPath: '/assets'
 
   /*
   Serve assets using CDN and Nginx:
